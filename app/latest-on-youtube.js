7c148f2a89db03281d4ab48c0cbb4583cdf5f763
Slobodan Gajic <freelancerslobodan@gmail.com>
2023-03-09 13:15:19 +0100

    update latest video

--- app/latest-on-youtube.js
+++ app/latest-on-youtube.js
@@ -10,7 +10,7 @@ function LatestOnYoutube() {
         <iframe
           width="560"
           height="315"
-          src="https://www.youtube.com/embed/co-m3bfIxKQ"
+          src="https://www.youtube.com/embed/ZFGX29ZI52U"
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen
