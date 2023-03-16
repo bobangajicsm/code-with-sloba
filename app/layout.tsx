c82e562e2961893983703fb01888dd1b8ccc6d79
Slobodan Gajic <freelancerslobodan@gmail.com>
2023-03-16 08:27:00 +0100

    remove old head

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,50 +70,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <title></title>
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="" />
-        <meta
-          property="og:image"
-          content="/og_image.png"
-        />
-        <meta property="og:title" content="Code with Sloba" />
-        <meta
-          property="og:description"
-          content="Learn JavaScript by doing through theory and practical projects"
-        />
-        <meta name="description" content="" key="desc" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@_SlobodanGajic_" />
-        <meta name="twitter:creator" content="@_SlobodanGajic_" />
-        <meta name="twitter:image" content="" />
-        <meta
-          name="twitter:image:src"
-          content="/og_image.png"
-        />
-        <meta charSet="utf-8" />
-        <meta
-          name="keywords"
-          content="code with sloba, javascript tutorials, frontend tips, software development"
-        ></meta>
-        <meta name="robots" content="index, follow" />
-
-        <meta content="Code with Sloba" />
-        <meta
-          name="twitter:description"
-          content="Learn JavaScript by doing through theory and practical projects"
-        />
-        <link rel="canonical" href="https://www.codewithsloba.com/" />
-        <meta name="next-head-count" content="18" />
-        <link
-          rel="icon"
-          type="image/x-icon"
-          sizes="60x61.66"
-          href="/favicon.ico"
-        />
-      </head>
       <body>
         <div>
           <Navigation />
