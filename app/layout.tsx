65db94747644f5ecd11e9af027286663a56f9fe1
Slobodan Gajic <freelancerslobodan@gmail.com>
2023-03-19 00:23:28 +0100

    update head

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,46 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>
+          Code with Sloba | Helping You Become a Better JavaScript Engineer
+        </title>
+        <meta
+          name="description"
+          content="Learn JavaScript by doing through theory and practical projects"
+        />
+
+        <meta property="og:url" content="https://codewithsloba.com/" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Code with Sloba | Helping You Become a Better JavaScript Engineer"
+        />
+        <meta
+          property="og:description"
+          content="Learn JavaScript by doing through theory and practical projects"
+        />
+        <meta
+          property="og:image"
+          content="https://codewithsloba.com/og_image.png"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="codewithsloba.com" />
+        <meta property="twitter:url" content="https://codewithsloba.com/" />
+        <meta
+          name="twitter:title"
+          content="Code with Sloba | Helping You Become a Better JavaScript Engineer"
+        />
+        <meta
+          name="twitter:description"
+          content="Learn JavaScript by doing through theory and practical projects"
+        />
+        <meta
+          name="twitter:image"
+          content="https://codewithsloba.com/og_image.png"
+        />
+      </head>
       <body>
         <div>
           <Navigation />
