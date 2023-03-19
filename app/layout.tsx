f8c09e7045ee2cba0ced6e25121f925472e738d5
Slobodan Gajic <freelancerslobodan@gmail.com>
2023-03-19 06:49:42 +0100

    fix compile bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import Navigation from "./navigation";
 import Footer from "./footer";
 import "./globals.css";
+import { Suspense } from "react";
+
+function SearchBarFallback() {
+  return <>Loading...</>;
+}
 
 export default function RootLayout({
   children,
@@ -9,49 +14,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <title>
-          Code with Sloba | Helping You Become a Better JavaScript Engineer
-        </title>
-        <meta
-          name="description"
-          content="Learn JavaScript by doing through theory and practical projects"
-        />
-
-        <meta property="og:url" content="https://codewithsloba.com/" />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:title"
-          content="Code with Sloba | Helping You Become a Better JavaScript Engineer"
-        />
-        <meta
-          property="og:description"
-          content="Learn JavaScript by doing through theory and practical projects"
-        />
-        <meta
-          property="og:image"
-          content="https://codewithsloba.com/og_image.png"
-        />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="codewithsloba.com" />
-        <meta property="twitter:url" content="https://codewithsloba.com/" />
-        <meta
-          name="twitter:title"
-          content="Code with Sloba | Helping You Become a Better JavaScript Engineer"
-        />
-        <meta
-          name="twitter:description"
-          content="Learn JavaScript by doing through theory and practical projects"
-        />
-        <meta
-          name="twitter:image"
-          content="https://codewithsloba.com/og_image.png"
-        />
-      </head>
       <body>
         <div>
-          <Navigation />
+          <Suspense fallback={<SearchBarFallback />}>
+            <Navigation />
+          </Suspense>
           {children}
           <Footer />
         </div>
