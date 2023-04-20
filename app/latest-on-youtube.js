d066b0676b9e9ae0a0c0bae83538deaf4380a6c8
Slobodan Gajic <freelancerslobodan@gmail.com>
2023-04-20 14:11:16 +0200

    update latest on youtube

--- app/latest-on-youtube.js
+++ app/latest-on-youtube.js
@@ -8,7 +8,7 @@ function LatestOnYoutube() {
     <div className={styles.latestOnYoutube}>
       <h2 className={styles.latestOnYoutubeTitle}>Trending On YouTube</h2>
       <Link
-        href="https://youtu.be/CahrHdgZ3Ls"
+        href="https://youtu.be/-ihkRNWStls"
         className={styles.latestOnYoutubeLink}
         target="_blank"
       >
@@ -16,7 +16,7 @@ function LatestOnYoutube() {
           className={styles.latestOnYoutubeImage}
           width={480}
           height={360}
-          src="https://img.youtube.com/vi/CahrHdgZ3Ls/hqdefault.jpg"
+          src="https://img.youtube.com/vi/-ihkRNWStls/hqdefault.jpg"
           alt="Code with Sloba YouTube"
         />
         <span className={styles.latestOnYoutubePlayButton}>▶</span>
